ec005f6edabb78390c2c4586b9f606d79b08cfb5
ARIEL NICOLLAS <39384032+arielnicollas@users.noreply.github.com>
2023-07-13 22:52:13 -0300

    Feature: Subindo Backend Deploy

---
+++ register.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+const getId = require(`../controllers/getUsersById`)
+const reqPost = require(`../controllers/postUsers`)
+const reqUpdate = require(`../controllers/updateUsers`)
+const reqDelete = require(`../controllers/deleteUsers`)
+
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+// app.get('/users', db.getUsers)
+app.get('/users/:id', getId.getUserById) 
+app.post('/users', reqPost.createUser)
+app.put('/users/:id', reqUpdate.updateUsers)
+app.delete('/users/:id', reqDelete.deleteUsers) 
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}.`)
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
